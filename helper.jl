08fe669d781d057b263d57b591e726a70f91cfdb
maximilian <maximilian.stefan.ernst@gmail.com>
2022-04-13 11:11:30 +0200

    first artifact

--- helper.jl
+++ helper.jl
@@ -1,21 +1,26 @@
-function new_artifact(name, artifact_toml, csv_dir, tarball_dir, args...)
+function new_artifact(name, artifact_toml, src_dir, tarball_dir, tarball_name, args...)
     # Query the `Artifacts.toml` file for the hash bound to the name "political_democracy"
     # new_hash = artifact_hash(name, artifact_toml)
 
     new_hash = create_artifact() do artifact_dir
         # move files into the artifact directory
         for arg in args
-            cp(joinpath(csv_dir, arg), joinpath(artifact_dir, arg))
+            cp(joinpath(src_dir, arg), joinpath(artifact_dir, arg))
         end
     end
 
-    bind_artifact!(artifact_toml, name, new_hash; force = true, lazy = true)
+    tarball_hash = archive_artifact(new_hash, joinpath(tarball_dir, tarball_name*".tar.gz"))
 
-    new_gz_hash = Dict{Any, String}()
-    for arg in args
-        push!(new_gz_hash, arg => archive_artifact(new_hash, joinpath(tarball_dir, arg*".tar.gz")))
-    end
+    bind_artifact!(
+        artifact_toml, 
+        name, 
+        new_hash; 
+        force = true, 
+        lazy = true, 
+        download_info = [(joinpath(download_root, replace(tarball_dir, pwd()*"/" => "")), tarball_hash)]
+    )
 
-    return new_gz_hash, new_hash
+    #return tarball_hash, new_hash
 end
 
+download_root = "https://github.com/StructuralEquationModels/Data"
