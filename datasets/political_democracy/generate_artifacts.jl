f0673213071885ce43df566ec68dfce5821c8409
maximilian <maximilian.stefan.ernst@gmail.com>
2022-04-22 00:11:47 +0200

    update political_democracy_solution

--- datasets/political_democracy/generate_artifacts.jl
+++ datasets/political_democracy/generate_artifacts.jl
@@ -9,8 +9,21 @@ new_artifact("political_democracy", artifact_toml, src_dir, tarball_dir, "data.c
 
 new_artifact("political_democracy_fiml", artifact_toml, src_dir, tarball_dir, "data_fiml.csv", "data_fiml.csv")
 
-new_artifact("political_democracy_solution", artifact_toml, src_dir, tarball_dir, "data.csv", "data.csv")
+new_artifact(
+    "political_democracy_solution", 
+    artifact_toml, src_dir, tarball_dir, 
+    "political_democracy_solution", 
+    "fitmeasures_ml.csv",
+    "fitmeasures_ls.csv",
+    "fitmeasures_ml_mean.csv",
+    "fitmeasures_ls_mean.csv",
+    "fitmeasures_fiml.csv",
+    "parameter_estimates_ml.csv",
+    "parameter_estimates_ls.csv",
+    "parameter_estimates_ml_mean.csv",
+    "parameter_estimates_ls_mean.csv",
+    "parameter_estimates_fiml.csv")
 
-new_artifact("political_democracy_solution_mean", artifact_toml, src_dir, tarball_dir, "data.csv", "data.csv")
+# new_artifact("political_democracy_solution_mean", artifact_toml, src_dir, tarball_dir, "data.csv", "data.csv")
 
-new_artifact("political_democracy_solution_fiml", artifact_toml, src_dir, tarball_dir, "data.csv", "data.csv")
+# new_artifact("political_democracy_solution_fiml", artifact_toml, src_dir, tarball_dir, "data.csv", "data.csv")
